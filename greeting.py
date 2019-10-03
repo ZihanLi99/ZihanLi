9f3b2cb862e39a4b5197cd9b87b165dec3d233e5
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-02 19:03:44 -0700

    Delete greeting.py

--- greeting.py
+++
@@ -1,6 +0,0 @@
-# Author: Zihan Li
-# Date: 2/10/2019
-# Description: Asks the user for their name and then greets them by name
-
-name = input("Please enter your name: ")
-print("Hello " + name + "!")
