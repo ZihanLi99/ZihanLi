67b0a56bc1bb38c847855daba42122941cec4da5
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-02 19:03:39 -0700

    Delete animal.py

--- animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Zihan Li
-# Date: 2/10/2019
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
