30a50b79d44e39af439995891b85edff7a8e8767
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-02 19:02:51 -0700

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,6 @@
+# Author: Zihan Li
+# Date: 2/10/2019
+# Description: Asks the user for their name and then greets them by name
+
+name = input("Please enter your name: ")
+print("Hello " + name + "!")
