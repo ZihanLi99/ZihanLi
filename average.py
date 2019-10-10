5ed42e72cf5c28bdad0a4c1b1683ca322b8cb0b3
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-09 19:31:44 -0700

    Update average.py

--- average.py
+++ average.py
@@ -1,3 +1,7 @@
+# Author: Zihan Li
+# Date: 8/10/2019
+# Description: asks the user for five numbers and then prints out the average of those numbers. 
+
 print("Please enter five numbers.")
 total = 0
 for x in range(5):
