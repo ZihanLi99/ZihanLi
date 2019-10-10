7bf44415b7d41491bb9d3686d3c57e1ff963f06d
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-09 19:33:32 -0700

    Update temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -1,3 +1,7 @@
+# Author: Zihan Li
+# Date: 8/10/2019
+# Description: converts Celsius temperatures to Fahrenheit temperatures
+
 print("Please enter a Celsius temperature.")
 val_c = input()
 val_f = (9/5)*float(val_c) + 32
