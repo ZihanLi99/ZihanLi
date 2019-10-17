38c691205b2ae3b756ddd444555f0332eeeee538
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-16 20:07:42 -0700

    Create num_guess.py

---
+++ num_guess.py
@@ -0,0 +1,25 @@
+# Author: Zihan Li
+# Date: 16/10/2019
+# Description: prompts the user for an integer that the player will try to guess.
+
+print("Enter the number for the player to guess. ")
+num = int(input())
+print("Enter your guess:.")
+
+guess_try = 1
+guess = False
+
+while (guess == False):
+    guess_num = int(input())
+
+    if (guess_num ==num):
+        guess = True
+        print("You guessed the number in ", guess_try, "attempts")
+
+    elif guess_num > num:
+        print("Too high - try again:")
+
+    else:
+        print("Too low - try again:")
+
+    guess_try = guess_try + 1
