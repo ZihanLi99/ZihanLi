0fe7e46a653fe8cc4d906a7ec4a463e418c3b0fd
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-23 19:44:16 -0700

    Create fall_distance.py

---
+++ fall_distance.py
@@ -0,0 +1,11 @@
+# Author: Zihan Li
+# Date: 22/10/2019
+# Description: determine the distance an object falls due to gravity in a specific time period
+
+def fall_distance(t):
+    d = (1/2) * 9.8 * t ** 2
+    return d
+
+print("Enter t:")
+t = float(input())
+print(fall_distance(t))
