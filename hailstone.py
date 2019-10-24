9249cb23f99ea62c67508ffdd0bb274f5a0135cb
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-23 22:39:04 -0700

    Update hailstone.py

--- hailstone.py
+++ hailstone.py
@@ -1,21 +1,15 @@
 # Author: Zihan Li
 # Date: 23/10/2019
-# Description:  takes a positive integer parameter as the initial number of
-#               a hailstone sequence and returns how many steps it takes to reach 1
+# Description: takes a positive integer parameter as the initial number of a hailstone sequence and returns how many steps it takes to reach 1
 
 
-def hailstone(num):
-    if num % 2 == 0:
-        return num = num // 2
-    elif num % 2 == 1:
-        return num = 3 * num + 1
-    
-print("Enter a number:")
-num = hauilstone(int(input())
-
-step = 1
-
-while (num != 1)
-    num = hailtone(num)
-    step = step + 1
-print(step)
+def hailstone():
+    num = int(input())
+    step = 0
+    while (num != 1):
+        if num % 2 == 0:
+            num = num // 2
+        elif num % 2 == 1:
+            num = 3 * num + 1
+        step = step + 1
+    print(step)
