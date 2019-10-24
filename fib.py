b9f09c7c75df1e416138957f6d813afb96240a7e
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-23 20:19:38 -0700

    Update fib.py

--- fib.py
+++ fib.py
@@ -3,14 +3,12 @@
 # Description: takes a positive integer parameter and returns the number at that position of the Fibonacci sequence.
 
 def fib(n):
-    if n == 1:
-        return 1
-    elif n == 2:
+    if n == 1 or n == 2:
         return 1
     elif n > 2:
         return fib(n - 1) + fib(n - 2)
 
 
-print("Enter n:")
-n = int(input())
-print(fib(n))
+# print("Enter n:")
+# n = int(input())
+# print(fib(n))
