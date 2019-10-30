e170a4815519c947db9ac499183fb7537cea8884
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-10-29 22:10:08 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,10 +1,10 @@
+# Author: Zihan Li
+# Date: 29/10/2019
+# Description: a recursive function named multiply that takes two positive integers as parameters and returns the product of those two numbers 
+
 def multiply(a,b):
-    """
-    Returns the greatest common divisor of a and b
-    """
+
     if b == 0:
         return 0
     else:
         return a + multiply(a, b - 1)
-
-print(multiply(7,4))
