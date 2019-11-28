8336edacdf5dad3bf80420af287a31892ba09e48
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2019-11-27 21:04:46 -0800

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,64 +1,64 @@
 class TicTacToe:
     def __init__(self) -> None:
         super().__init__()
-        self._board = None
-        self._current_state = None
+        self.board = None
+        self.current_state = None
 
     def init(self):
-        self._board = [
+        self.board = [
             ['','',''],
             ['','',''],
             ['','',''],
         ]
-        self._current_state = 'UNFINISHED'
+        self.current_state = 'UNFINISHED'
 
     def get_current_state(self):
-        return self._current_state
+        return self.current_state
 
     def make_move(self, row, col, player):
-        if 0<=row<=2 and 0<=col<=2 and self._board[row][col]=='' and self._current_state=='UNFINISHED':
-            self._board[row][col] = player
+        if 0<=row<=2 and 0<=col<=2 and self.board[row][col]== '' and self.current_state== 'UNFINISHED':
+            self.board[row][col] = player
             self.check_status()
             return True
         return False
 
     def check_status(self):
         for r in range(3):
-            if self._board[r][0]==self._board[r][1]==self._board[r][2] and self._board[r][0]!='':
-                if self._board[r][0]=='x':
-                    self._current_state='X_WON'
+            if self.board[r][0]==self.board[r][1]==self.board[r][2] and self.board[r][0]!= '':
+                if self.board[r][0]== 'x':
+                    self.current_state= 'X_WON'
                 else:
-                    self._current_state='O_WON'
+                    self.current_state= 'O_WON'
                 return
         for c in range(3):
-            if self._board[0][c]==self._board[1][c]==self._board[2][c] and self._board[0][c]!='':
-                if self._board[0][c]=='x':
-                    self._current_state='X_WON'
+            if self.board[0][c]==self.board[1][c]==self.board[2][c] and self.board[0][c]!= '':
+                if self.board[0][c]== 'x':
+                    self.current_state= 'X_WON'
                 else:
-                    self._current_state='O_WON'
+                    self.current_state= 'O_WON'
                 return
-        if self._board[0][0]==self._board[1][1]==self._board[2][2] and self._board[0][0]!='':
-            if self._board[0][0]=='x':
-                self._current_state='X_WON'
+        if self.board[0][0]==self.board[1][1]==self.board[2][2] and self.board[0][0]!= '':
+            if self.board[0][0]== 'x':
+                self.current_state= 'X_WON'
             else:
-                self._current_state='O_WON'
+                self.current_state= 'O_WON'
             return
-        if self._board[0][2]==self._board[1][1]==self._board[2][0] and self._board[1][1]!='':
-            if self._board[1][1]=='x':
-                self._current_state='X_WON'
+        if self.board[0][2]==self.board[1][1]==self.board[2][0] and self.board[1][1]!= '':
+            if self.board[1][1]== 'x':
+                self.current_state= 'X_WON'
             else:
-                self._current_state='O_WON'
+                self.current_state= 'O_WON'
             return
 
         for r in range(3):
             for c in range(3):
-                if self._board[r][c]=='':
-                    self._current_state = 'UNFINISHED'
+                if self.board[r][c]== '':
+                    self.current_state = 'UNFINISHED'
                     return
-        self._current_state = 'DRAW'
+        self.current_state = 'DRAW'
 
     def print_board(self):
         for r in range(3):
             for c in range(3):
-                print('{:1s}'.format(self._board[r][c]), end='')
+                print('{:1s}'.format(self.board[r][c]), end='')
             print()
