de85c89bf24adac16742b1756d10b14ba9c6afa3
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-01-29 23:25:28 -0800

    Update bin_except.py

--- bin_except.py
+++ bin_except.py
@@ -1,35 +1,24 @@
-class TargetNotFound(Exception):
-    def __init__(self, val):
-        self.val = val
-    def __str__(self):
-        return(repr(self.val))
-
-def binarySearch(arr, searchItem):
-    size = len(arr)
-    beg = 0
-    end = size - 1
-    while(beg < end):
-        mid = (beg + end) // 2
-        if arr[mid] == searchItem:
-            return mid
-        elif arr[mid] > searchItem:
-            end = mid - 1
+class TargetNotFoundException(Exception):
+    pass
+def bin_except(arr,target):
+    low = 0
+    high = len(arr)-1
+    found = False
+    index=-1
+    while( low<=high and not found):
+        mid = (low + high)//2 # integer division by 2
+        if arr[mid] == target: # target is equal than arr[mid]
+            found=True
+            index=mid
         else:
-            beg = mid + 1
-    raise TargetNotFound("TargetNotFound")
-
-arr = []
-print("Enter the numbers -1 to stop")
-while(True):
-    num = int(input(""))
-    if num == -1:
-        break
-    arr.append(num)
-arr.sort()
-print("After sorting your list is : ", arr)
-searchItem = int(input("Enter the number to search:"))
-try:
-    ind = binarySearch(arr, searchItem)
-    print("Your number is found in the list at the index {}".format(ind))
-except TargetNotFound as error:
-    print(error.val)
+            if target < arr[mid]: # target is less than arr[mid]
+                high = mid - 1
+            else: # target is greater than arr[mid]
+                low = mid + 1
+    try:
+        if not found:
+            raise TargetNotFoundException
+        else:
+            print("Target is Found at index: ",index)
+    except TargetNotFoundException:
+        print("Target Is Not Found in array ")
