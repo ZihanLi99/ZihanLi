b2cdf8a44044078d68350a2b25f1fef2e80cd46d
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-01-29 23:43:58 -0800

    Update bin_except.py

--- bin_except.py
+++ bin_except.py
@@ -3,7 +3,6 @@
 # Description: a Box class whose init method takes three parameters and uses them to
 #              initialize the length, width and height of a Box
 
-from math import floor
 class TargetNotFound(Exception):
     pass
 
@@ -11,11 +10,13 @@ def bin_except(arr, tar_val):
     min = 0
     max = len(min) - 1
     while min <= max:
-        mid = floor((min + max) / 2)
-        if arr[mid] == tar_val:
-            return mid
-        if arr[mid] > tar_val:
-            max = mid - 1
-        else:
-            min = mid + 1
-    raise TargetNotFound("Can't find the number.")
+        try:
+            mid = (min + max) // 2
+            if arr[mid] == tar_val:
+                return mid
+            if arr[mid] > tar_val:
+                max = mid - 1
+            else:
+                min = mid + 1
+        except TargetNotFound:
+            print("Can't find the number.")
