49d6778f3294b1684cb103c255b4b0d3a692b4cd
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-02-05 01:29:57 -0800

    Update file_sum.py

--- file_sum.py
+++ file_sum.py
@@ -9,8 +9,8 @@ def file_sum(file_name):
     with open(file_name, 'r') as infile:
     # open a file you want
         for num in infile:
-            sum = sum + float(num)
+            sum = sum + str(num)
     # use loop to calculate sum of these number
     with open('sum.txt', 'w') as outfile:
-        outfile.write("Sum = " + str(sum))
+        outfile.write(str(sum) + '\n')
     # create a new file to show the sum
