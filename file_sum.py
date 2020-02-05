e25b8ac5c0a1f2127cd40c7b8845e59da99a26c3
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-02-05 01:16:30 -0800

    Create file_sum.py

---
+++ file_sum.py
@@ -0,0 +1,16 @@
+# Author: Zihan Li
+# Date: 2020/2/4
+# Description: a function named file_sum that takes as a parameter the name of a text
+#              file that contains a list of numbers, one to a line
+
+def file_sum(file_name):
+    sum = 0
+    # instalize the data
+    with open(file_name,'r') as infile:
+    # open a file you want
+        for num in infile:
+            sum = sum + num
+    # use loop to calculate sum of these number
+    with open('sum.txt', 'w') as outfile:
+        outfile.write("Sum = ", str(sum))
+    # create a new file to show the sum
