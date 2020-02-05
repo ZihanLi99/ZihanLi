e1881c8a83ce72e493644068a6f2e863535deba6
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-02-05 02:39:59 -0800

    Create SatData.py

---
+++ SatData.py
@@ -0,0 +1,32 @@
+# Author: Zihan Li
+# Date: 2020/2/4
+# Description: a class named SatData that reads a JSON file containing data on 2010 SAT results for New York City
+#              and writes the data to a text file in CSV (comma-separated values) format
+
+import json
+
+class SatData:
+    def __init__(self):
+        super().__init__()
+        self.idmap = {}
+        # declare a dict to store id and row
+        
+        with open('sat.json', 'r') as fr:
+            self.data = json.load(fr)
+            for row in self.data['data']:
+                id = row[8]
+                # get it's id and store it to idmap for each row
+                self.idmap[id] = row
+
+    def save_as_csv(self, dbns):
+        columns = ['DBN','School Name','Number of Test Takers','Critical Reading Mean','Mathematics Mean','Writing Mean']
+        # save columns
+        with open('output.csv', 'w') as fr:
+            fr.write(','.join(columns)+"\n")
+            # open file for write
+            for dbn in dbns:
+                if dbn in self.idmap:
+                    line = ','.join(self.idmap[dbn][8:])
+                    # read column values, generate a record
+                    fr.write(line+'\n')
+                    # write to csv file
