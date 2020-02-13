cb3ff88b104d38adf185a7090f4bf4a06b16c51b
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-02-12 19:30:55 -0800

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,12 +1,13 @@
 # Author: Zihan Li
 # Date: 2020/2/12
-# Description: Write a recursive function named is_palindrome that takes as its parameter a string
+# Description: Write a recursive function named is_palindrome that takes as its parameter a string.
+#              It should return True if that string is a palindrome, but return False otherwise.
 
-def isPalindrome(word_str):
+def is_palindrome(word_str):
     if len(word_str) == 1:
         # if there is only one letter, return True
         return True
     if word_str[0] != word_str[-1]:
         # compare the first and last letter
         return False
-    return isPalindrome(word_str[1:-1])
+    return is_palindrome(word_str[1:-1])
