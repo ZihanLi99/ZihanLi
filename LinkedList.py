e4b4d6c062c390a78b1477d9217d9a8f381b50f5
ZihanLi99 <56095602+ZihanLi99@users.noreply.github.com>
2020-02-18 16:50:02 -0800

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,11 +1,14 @@
+# Author: Zihan Li
+# Date: 2020/2/18
+# Description: a LinkedList class that has recursive implementations of the add, display, 
+#              and remove methods described in the lesson.
+
 class Node:
-    
     def __init__(self, data):
         self.data = data
         self.next = None
 
 class LinkedList:
-    
     def __init__(self):
         self.head = None
 
@@ -22,35 +25,35 @@ class LinkedList:
         self.add_rec(self.head, val)
 
     def display_rec(self, node):
-        # if current node is None, print a empty line
         if node is None:
+        # if current node is None, print a empty line
             print()
         else:
-            # print current node's data
             print(node.data, end=" ")
-            # display from next node
+            # print current node's data
             self.display_rec(node.next)
+            # display from next node
 
     def display(self):
         self.display_rec(self.head)
 
     def remove_rec(self, prev, node, val):
-        # if current node is None, do nothing
         if node is None:
+        # if current node is None, do nothing
             return
         else:
-            # if val found
             if node.data == val:
-                # remove it
+            # if val found
                 if prev is not None:
                     prev.next = node.next
+                    # remove it
                 else:
                     self.head = node.next
-                # remove from next
                 self.remove_rec(prev, node.next, val)
-            else:
                 # remove from next
+            else:
                 self.remove_rec(node, node.next, val)
+                # remove from next
 
     def remove(self, val):
         self.remove_rec(None, self.head, val)
@@ -59,16 +62,16 @@ class LinkedList:
         return self.head is None
 
     def contains_rec(self, node, val):
-        # if current node is none, return False
         if node is None:
+        # if current node is none, return False
             return False
         else:
-            # if val found, return True
             if node.data == val:
+            # if val found, return True
                 return True
             else:
-                # find val from next node
                 return self.contains_rec(node.next, val)
+                # find val from next node
 
     def contains(self, val):
         return self.contains_rec(self.head, val)
@@ -77,22 +80,22 @@ class LinkedList:
         pass
 
     def reverse_rec(self, node):
-        # if node is None, do nothing
         if node is None:
+        # if node is None, do nothing
             return None
 
         next = node.next
         node.next = None
 
-        # if next node is None, return current node
         if next is None:
+        # if next node is None, return current node
             return node
 
-        # reverse from next node
         subhead = self.reverse_rec(next)
+        # reverse from next node
         subtail = next
-        # add current node to the end of the reversed linked list
         subtail.next = node
+        # add current node to the end of the reversed linked list
         return subhead
 
     def reverse(self):
